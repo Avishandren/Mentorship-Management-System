08e37a422e96c48fb52b67ad3cd794cccf0bbe8f
Andile Mahlaba <mahlabaandile59@gmail.com>
2025-03-25 01:39:33 +0200

    Deleted unnecessary files and kept only the required files

--- mentorverification/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'supersecretkey'
-    # Update the SQLite database URI to the correct format
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
